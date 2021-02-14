3a8be80e79397b179873ed3dc5e26c3638611fb2
y2k-shubham <y2k.shubhamgupta@gmail.com>
2021-02-14 16:21:59 +0530

    [fix] (cache_1_self_1) Fix return type of 'get' function

--- cache_1_self_1/src/main/java/Cache/ICache.java
+++ cache_1_self_1/src/main/java/Cache/ICache.java
@@ -4,7 +4,7 @@ public interface ICache {
 
     public boolean put(String key, String value);
 
-    public boolean get(String key);
+    public String get(String key);
 
     public boolean clear(String key);
 
